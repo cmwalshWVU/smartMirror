8bbce133b8707fcc181d8c53a8d9b79651654ac8
cmwalshWVU <cmwalsh@mix.wvu.edu>
2017-04-17 19:09:46 -0400

    Update applemusic.js

--- modules/applemusic/applemusic.js
+++ modules/applemusic/applemusic.js
@@ -9,7 +9,7 @@ Module.register("applemusic", {
     getDom: function() {
         var iframe = document.createElement("IFRAME");
         iframe.frameborder = 0;
-        iframe.src =  "//tools.applemusic.com/embed/v1/playlist/pl.2b0e6e332fdf4b7a91164da3162127b5?country=us";
+        iframe.src =  "https://embed.spotify.com/?uri=spotify:album:4eLPsYPBmXABThSJ821sqY";
         return iframe;
     }
 });
