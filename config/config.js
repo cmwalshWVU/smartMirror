d1309677519ef494703fcdd68e9fbd1ba88721e3
cmwalshWVU <cmwalsh@mix.wvu.edu>
2017-04-17 18:49:22 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -38,7 +38,7 @@ var config = {
         },
         {
             module: 'MMM-SoccerLiveScore',
-            position: 'top_left',
+            position: 'bottom_left',
             header: 'Live-Scores',
             config: {
                 leagues: [1],
@@ -142,22 +142,22 @@ var config = {
                 fade: true,
                 maxCharacters: 50
             }
-        },
-        {
-            module: 'email',
-            position: 'bottom_left',
-            header: 'Email',
-            config: {
-                user: 'cmwalsh@mix.wvu.edu',
-                password: 'Odindog1.',
-                host: 'imap.gmail.com',
-                port: 993,
-                tls: true,
-                authTimeout: 10000,
-                numberOfEmails: 3,
-                fade: true
-            }
         }
+//         {
+//             module: 'email',
+//             position: 'bottom_left',
+//             header: 'Email',
+//             config: {
+//                 user: 'cmwalsh@mix.wvu.edu',
+//                 password: 'Odindog1.',
+//                 host: 'imap.gmail.com',
+//                 port: 993,
+//                 tls: true,
+//                 authTimeout: 10000,
+//                 numberOfEmails: 3,
+//                 fade: true
+//             }
+//         }
     ]
 };
 
